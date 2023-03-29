4c1cbca5bd03e50a8fa6497a63db6330ceca215b
Aleksey Alekseev <AlekseevFr@yandex.ru>
2023-03-29 23:53:11 +0300

    2

--- backend/middlewares/cross.js
+++ backend/middlewares/cross.js
@@ -1,10 +1,14 @@
 function handleCross(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', true);
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Credentials', true);
   const { method } = req;
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   const requestHeaders = req.headers['access-control-request-headers'];
   if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
     res.setHeader('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.setHeader('Access-Control-Allow-Headers', requestHeaders);
     return res.end();
