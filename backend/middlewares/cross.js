84b6aff7d76c6e6d6b0d15f9f9123ce62d68e4db
Aleksey Alekseev <AlekseevFr@yandex.ru>
2023-03-29 23:34:53 +0300

    fix cross

--- backend/middlewares/cross.js
+++ backend/middlewares/cross.js
@@ -1,16 +1,13 @@
-const allowedCors = ['http://alekseevfr.students.nomoredomains.work', 'http://api.alekseevfr.students.nomoredomains.work', 'https://alekseevfr.students.nomoredomains.work', 'https://api.alekseevfr.students.nomoredomains.work', 'http://localhost:3000'];
 function handleCross(req, res, next) {
   const { origin } = req.headers;
-  if (allowedCors.includes(origin)) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
-    res.setHeader('Access-Control-Allow-Credentials', true);
-  }
+  res.header('Access-Control-Allow-Origin', origin);
+  res.header('Access-Control-Allow-Credentials', true);
   const { method } = req;
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   const requestHeaders = req.headers['access-control-request-headers'];
   if (method === 'OPTIONS') {
-    res.setHeader('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.setHeader('Access-Control-Allow-Headers', requestHeaders);
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
     return res.end();
   }
   return next();
