a33edb76e81c39c1818b02a20d96e43cf1c640e1
Aleksey Alekseev <AlekseevFr@yandex.ru>
2023-03-30 21:04:49 +0300

    new cross2

--- backend/middlewares/cross.js
+++ backend/middlewares/cross.js
@@ -1,28 +1,18 @@
 const allowedCors = ['https://alekseevfr.students.nomoredomains.work', 'http://alekseevfr.students.nomoredomains.work', 'localhost:3000'];
-
 function handleCross(req, res, next) {
   const { origin } = req.headers;
-
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
-  } else {
     res.header('Access-Control-Allow-Origin', '*');
   }
-
   const { method } = req;
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   const requestHeaders = req.headers['access-control-request-headers'];
-
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-
-    if (requestHeaders) {
-      res.header('Access-Control-Allow-Headers', requestHeaders);
-    }
-    next();
-  } else {
-    next();
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
   }
+  return next();
 }
-
 module.exports = { handleCross };
