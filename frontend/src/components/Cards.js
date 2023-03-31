a02b9466823e11a95d76a4a0aa9ce7a5a103a88d
Aleksey Alekseev <AlekseevFr@yandex.ru>
2023-03-31 19:12:00 +0300

    Port change2

--- frontend/src/components/Cards.js
+++ frontend/src/components/Cards.js
@@ -59,7 +59,9 @@ function Cards({email, onLeave}) {
   function handleCardLike(card) {
     const isLiked = card.likes.some(i => i._id === currentUser._id);
     api.changeLikeCardStatus(card._id, isLiked).then((newCard) => {
-      setCards((state) => state.map((c) => c._id === card._id ? newCard.data : c));
+      setCards((state) => {
+        console.log('check', state, card, newCard)
+        return state.map((c) => c._id === card._id ? newCard.data : c)});
     }).catch(console.error);
   }
   function handleCardDelete(card) {
