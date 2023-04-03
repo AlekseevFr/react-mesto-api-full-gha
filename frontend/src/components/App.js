b24f47545d0c8b0473b268bb8cb8c2ed8f134dea
Aleksey Alekseev <AlekseevFr@yandex.ru>
2023-04-04 00:46:51 +0300

    review

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -38,7 +38,7 @@ function checkToken(token) {
   function handleRegister(data) {
     userapi.register(data).then(() => {
       setType('success');
-      history.push("/sign-in");
+      setTimeout(() => history.push("/sign-in"), 2000);
     }).catch(()=> setType('error'));
   }
 
