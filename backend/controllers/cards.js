65f8cf01509a5200b3ce7cdbc7a0ef9914273dce
Aleksey Alekseev <AlekseevFr@yandex.ru>
2023-04-03 23:27:23 +0300

    cards

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -6,7 +6,7 @@ const { NotFound } = require('../errors/NotFound');
 const { Forbidden } = require('../errors/Forbidden');
 
 const getCards = (req, res, next) => {
-  Card.find({}).sort('-createdAt')
+  Card.find({}).sort({ createdAt: -1 })
     .populate('owner')
     .then((cards) => res.status(constants.HTTP_STATUS_OK).send({ data: cards }))
     .catch(next);
